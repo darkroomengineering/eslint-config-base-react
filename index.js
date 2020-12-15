7724d0cb7a8d090d70813d0e66ade1ce4e08e1d2
Franco Arza <franco@studiofreight.com>
2020-12-15 20:09:28 -0300

    basic linting rules with prettier added

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: [
+    "react-app", // or whatever you chose as your base config
+    "plugin:prettier/recommended", // this will stop Prettier and ESLint from fighting over fixes
+    "prettier/react" // optionally include this if you use React
+  ],
+  plugins: ["prettier"],
+  ignorePatterns: ["node_modules", ".next", ".now"],
+  rules: {
+    // make Prettier return errors
+    "prettier/prettier": [
+      "error",
+      {
+        // Optional Prettier config changes
+        useTabs: false,
+        tabWidth: 2,
+        printWidth: 80,
+        singleQuote: false,
+        trailingComma: "none",
+        jsxBracketSameLine: false,
+        semi: false
+      }
+    ]
+  }
+}
